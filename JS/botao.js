b134235f24ea8b2c11989831109753325b077e8c
Italo Luan de Almeida <contato.almeidagrupo@gmail.com>
2026-04-17 09:52:56 -0400

    TASK4

--- JS/botao.js
+++
@@ -1,4 +0,0 @@
-const menu__icon = document.querySelector(".menu__icon");
-const nav = document.querySelector(".nav");
-
-menu__icon.addEventListener("click", () => nav.classList.toggle("active"));
---
+++ JS/forms.js
@@ -0,0 +1,17 @@
+function enviarWhats(event){
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5567999435703';
+
+    const texto = `Olá Me chamo ${nome}, ${mensagem}`
+    const msgFormatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    console.log(url)
+
+    window.open(url, '_blank')
+
+}
